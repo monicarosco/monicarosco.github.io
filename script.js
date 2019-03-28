36b3860304d4190ed5e1ac1200932c6811a28283
Monica Silbaugh <monicarosco@gmail.com>
2019-03-27 19:59:51 -0500

    Make website

---
+++ script.js
@@ -0,0 +1,14 @@
+// This is a function.
+function doSomething() {
+	alert("Hello, World");
+}
+// This is a function.
+function doSomething() {
+	document.getElementById("field").style = "background:#0000FF";
+}
+
+// This is a function.
+function doSomethingElse() {
+	document.getElementById("field").style = "background:#FF0000";
+}
+
